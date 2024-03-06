dfb66812ef9854b194b79aac9bf3288521400aa0
Amarildo <you@example.com>
2024-03-06 18:50:26 +0100

    added links

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/classe', function () {
+    $class = 116;
+    $students = [
+        'alunno1' => 'Giovanni',
+        'alunno2' => 'pasquale',
+    ];
+    $data = compact('class', 'students');
+    return view('classe.lista_alunni', $data);
+})->name('classe');
